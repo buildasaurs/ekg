d5764c7635dfb2c38754ca8102645f9e202f1e70
Honza Dvorsky <honza@swiftkey.com>
2015-08-31 22:42:33 +0100

    fixes

--- routes/beep.js
+++ routes/beep.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* POST a heartbeat event. */
 router.post('/', function(req, res, next) {
-  res.send('roger that');
+  res.send('roger that: \n' + JSON.stringify(req.body, null, '\t'));
+});
+
+/* GET all events */
+router.get('/all', function(req, res, next) {
+  res.send('not implemented yet, my bad');
 });
 
 module.exports = router;
